dea025815bae574c9beababb1cb5a96dcb4fe1a6
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-02-12 19:40:29 -0300

    Configurando lint

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CabecalhoComponent,
-    RodapeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CabecalhoComponent,
+		RodapeComponent,
+		CriarPensamentoComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule, FormsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-criar-pensamento',
+	templateUrl: './criar-pensamento.component.html',
+	styleUrls: ['./criar-pensamento.component.scss'],
+})
+export class CriarPensamentoComponent implements OnInit {
+	pensamento = {
+		id: '1',
+		conteudo: 'Aprendendo Angular',
+		autoria: 'Dev',
+		modelo: 'modelo1',
+	};
+
+	constructor() {}
+
+	criarPensamento(): void {
+		alert('Novo pensamento criado!');
+	}
+
+	cancelarPensamento(): void {
+		alert('Cancelar');
+	}
+	ngOnInit(): void {
+		return;
+	}
+}
