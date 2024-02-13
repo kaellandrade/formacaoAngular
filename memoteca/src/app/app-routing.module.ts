95db3eb7a6639b6df55d132254d353e9f3c3a8cc
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-02-12 21:17:37 -0300

    Configurando rotas no Angular.

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CriarPensamentoComponent} from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
+import {ListarPensamentoComponent} from "./componentes/pensamentos/listar-pensamento/listar-pensamento.component";
 
-const routes: Routes = [];
+// Configurando minhas rotas
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'listarPensamento',
+        pathMatch: 'full'
+    },
+    {
+        path: 'criarPensamento',
+        component: CriarPensamentoComponent
+    },
+    {
+        path: 'listarPensamento',
+        component: ListarPensamentoComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
