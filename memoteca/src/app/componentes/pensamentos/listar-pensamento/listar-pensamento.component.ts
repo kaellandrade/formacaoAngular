94870c83b22bb817b30803170c2e59422ede273f
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-02-14 17:28:03 -0300

    Configurando pensamento listar-pensamento.component.

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ModelosPensamentos } from '../../../../enums/ModelosPensamentos';
-import { Pensamento } from '../../../../enums/Pensamento';
+import { Pensamento } from '../../../../interfaces/Pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -8,29 +8,17 @@ import { Pensamento } from '../../../../enums/Pensamento';
   styleUrls: ['./listar-pensamento.component.scss'],
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos: Pensamento[] = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: ModelosPensamentos.MODELO1,
-    },
-    {
-      conteudo: 'Minha prop é decorada com @Input()',
-      autoria: 'Compoenente filho',
-      modelo: ModelosPensamentos.MODELO2,
-    },
-    {
-      conteudo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
-       specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
-       unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
-       with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-      autoria: 'Compoenente filho',
-      modelo: ModelosPensamentos.MODELO3,
-    },
-  ];
+  listaPensamentos: ReadonlyArray<Pensamento> = [];
 
-  constructor() {}
+  constructor(private service: PensamentoService) {}
 
-  ngOnInit(): void {}
+  /**
+   * Ciclo de vida do componente.
+   */
+  ngOnInit(): void {
+    this.service.listar();
+    this.service.listar().subscribe((listaPensamentos: Pensamento[]) => {
+      this.listaPensamentos = listaPensamentos;
+    });
+  }
 }
