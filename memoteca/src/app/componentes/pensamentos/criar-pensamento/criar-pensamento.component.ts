1ba5dd7597b203e5abbe41f69fccdfc1c23840e0
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-02-14 17:27:35 -0300

    Configurando pensamento criar-pensamento.component.

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,30 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+import { ModelosPensamentos } from '../../../../interfaces/ModelosPensamentos';
+import { Pensamento } from '../../../../interfaces/Pensamento';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-criar-pensamento',
-    templateUrl: './criar-pensamento.component.html',
-    styleUrls: ['./criar-pensamento.component.scss'],
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.scss'],
 })
 export class CriarPensamentoComponent implements OnInit {
-    pensamento = {
-        id: '1',
-        conteudo: 'Aprendendo Angular',
-        autoria: 'Dev',
-        modelo: 'modelo1',
-    };
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: ModelosPensamentos.MODELO3,
+  };
 
-    constructor() {
-    }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) {}
 
-    criarPensamento(): void {
-        alert('Novo pensamento criado!');
-    }
+  criarPensamento(): void {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']).then(r => {
+        alert('Pensamento criado com sucesso!');
+      });
+    });
+  }
 
-    cancelarPensamento(): void {
-        console.log('Cancelar');
-    }
+  cancelarPensamento(): void {
+    this.router.navigate(['/listarPensamento']).then(r => {});
+  }
 
-    ngOnInit(): void {
-        return;
-    }
+  ngOnInit(): void {
+    return;
+  }
 }
