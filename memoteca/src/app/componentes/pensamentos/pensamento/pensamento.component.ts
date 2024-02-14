e44a7b2716e8d311f4866569ee1f6c6bb28adbc3
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-02-14 17:26:52 -0300

    Configurando pensamento componente.

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Pensamento } from '../../../../enums/Pensamento';
-import { ModelosPensamentos } from '../../../../enums/ModelosPensamentos';
+import { Pensamento } from '../../../../interfaces/Pensamento';
+import { ModelosPensamentos } from '../../../../interfaces/ModelosPensamentos';
 
 @Component({
   selector: 'app-pensamento',
@@ -10,17 +10,20 @@ import { ModelosPensamentos } from '../../../../enums/ModelosPensamentos';
 export class PensamentoComponent implements OnInit {
   private static readonly TAMANHO_MAXIMO_STRING = 256;
   @Input()
-  pensamento: Pensamento;
+  public pensamento: Pensamento;
 
   constructor() {
     this.pensamento = {
+      id: '0',
       conteudo: '',
       modelo: ModelosPensamentos.MODELO1,
       autoria: '',
     };
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    return;
+  }
 
   public larguraPensamento(): string {
     if (
