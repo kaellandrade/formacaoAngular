4e31d2620cc362dae4d5f5bfe06be08e9c87f1b1
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-02-16 17:39:32 -0300

    Configurando formulário reativo com FormBuilder e foFormGroup.

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,50 +1,44 @@
-import {Component, OnInit} from '@angular/core';
-import {PensamentoService} from '../pensamento.service';
-import {ModelosPensamentos} from '../../../../interfaces/ModelosPensamentos';
-import {Pensamento} from '../../../../interfaces/Pensamento';
-import {Router} from '@angular/router';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+import { ModelosPensamentos } from '../../../../interfaces/ModelosPensamentos';
+import { Pensamento } from '../../../../interfaces/Pensamento';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-    selector: 'app-criar-pensamento',
-    templateUrl: './criar-pensamento.component.html',
-    styleUrls: ['./criar-pensamento.component.scss'],
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.scss'],
 })
 export class CriarPensamentoComponent implements OnInit {
-    public modeloscards = ModelosPensamentos;
+  public modeloscards = ModelosPensamentos;
+  formulario!: FormGroup;
 
-    pensamento: Pensamento = {
-        conteudo: '',
-        autoria: '',
-        modelo: ModelosPensamentos.MODELO3,
-    };
-    formulario!: FormGroup;
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) {
+  }
 
-    constructor(
-            private service: PensamentoService,
-            private router: Router,
-            private formBuilder: FormBuilder
-    ) {
-    }
+  criarPensamento(): void {
+    this.service.criar(this.formulario.value).subscribe(() => {
+      this.router.navigate(['/listarPensamento']).then(r => {
+        alert('Pensamento criado com sucesso!');
+      });
+    });
+  }
 
-    criarPensamento(): void {
-        this.service.criar(this.pensamento).subscribe(() => {
-            this.router.navigate(['/listarPensamento']).then(r => {
-                alert('Pensamento criado com sucesso!');
-            });
-        });
-    }
+  cancelarPensamento(): void {
+    this.router.navigate(['/listarPensamento']).then(r => {
+    });
+  }
 
-    cancelarPensamento(): void {
-        this.router.navigate(['/listarPensamento']).then(r => {
-        });
-    }
-
-    ngOnInit(): void {
-        this.formulario = this.formBuilder.group({
-            conteudo: ['Formulário reativo'],
-            autoria: [''],
-            modelo: [ModelosPensamentos.MODELO1]
-        })
-    }
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: ['Linus Torvalds'],
+      modelo: [ModelosPensamentos.MODELO1],
+    });
+  }
 }
