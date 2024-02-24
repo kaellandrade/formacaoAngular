bfae2eb6d14e1348fbacc7f38237ece169c1aca2
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-02-24 18:28:16 -0300

    feat: Implementando funcionalidade de busca de pensamentos

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../../../../interfaces/Pensamento';
 import { PensamentoService } from '../pensamento.service';
-import { PensamentoRequestShape } from 'src/interfaces/PensamentoRequestShape';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Component({
 	selector: 'app-listar-pensamento',
@@ -9,10 +9,12 @@ import { PensamentoRequestShape } from 'src/interfaces/PensamentoRequestShape';
 	styleUrls: ['./listar-pensamento.component.scss'],
 })
 export class ListarPensamentoComponent implements OnInit {
-	listaPensamentos: Array<Pensamento> = [];
-	totalPensamentos: number = 0;
-	paginaAtual: number = 1;
-	next: boolean = false;
+	private paginaAtual: number = 1;
+	private readonly TAMANHO_MINIMO_PALAVRA_BUSCA = 4;
+	public listaPensamentos: Array<Pensamento> = [];
+	public totalPensamentos: number = 0;
+	public next: boolean = false;
+	public filtro: string = '';
 
 	constructor(private service: PensamentoService) {}
 
@@ -20,22 +22,57 @@ export class ListarPensamentoComponent implements OnInit {
 	 * Ciclo de vida do componente.
 	 */
 	public ngOnInit(): void {
-		this.service
-			.listar(this.paginaAtual)
-			.subscribe((listaPensamentos: PensamentoRequestShape) => {
-				this.totalPensamentos = listaPensamentos.items;
-				this.listaPensamentos = listaPensamentos.data;
-				this.next = Boolean(listaPensamentos.next);
-			});
+		this.carregarTodosPensamentos();
 	}
 
 	public carregarMaisPensamentos(): void {
+		if (this.next) {
+			this.service
+				.listar(++this.paginaAtual, this.filtro.trim())
+				.subscribe((response: HttpResponse<Pensamento[]>) => {
+					if (response.body) {
+						this.totalPensamentos = this.listaPensamentos.length;
+						this.listaPensamentos.push(...response.body);
+						this.next = this.hasNext(response.headers);
+					}
+				});
+		}
+	}
+
+	public filtrarPensamentos(): void {
+		if (this.filtro.trim().length >= this.TAMANHO_MINIMO_PALAVRA_BUSCA) {
+			this.service
+				.listar(1, this.filtro.trim())
+				.subscribe((response: HttpResponse<Pensamento[]>) => {
+					if (response.body) {
+						this.listaPensamentos = response.body;
+						this.totalPensamentos = this.listaPensamentos.length;
+						this.next = this.hasNext(response.headers);
+					}
+				});
+		}
+		if (this.filtro.trim().length === 0) {
+			this.carregarTodosPensamentos();
+		}
+	}
+
+	private hasNext(httpsHeaders: HttpHeaders): boolean {
+		if (httpsHeaders.get('Link')?.length) {
+			const links: Array<string> = httpsHeaders.get('Link')?.split(', ') || [];
+			return links.some(link => link.includes('rel="next"'));
+		}
+		return false;
+	}
+
+	private carregarTodosPensamentos(): void {
 		this.service
-			.listar(++this.paginaAtual)
-			.subscribe((listaPensamentos: PensamentoRequestShape) => {
-				this.totalPensamentos = listaPensamentos.items;
-				this.listaPensamentos.push(...listaPensamentos.data);
-				this.next = Boolean(listaPensamentos.next);
+			.listar(1, '')
+			.subscribe((response: HttpResponse<Pensamento[]>) => {
+				if (response.body) {
+					this.listaPensamentos = response.body;
+					this.totalPensamentos = this.listaPensamentos.length;
+					this.next = this.hasNext(response.headers);
+				}
 			});
 	}
 }
