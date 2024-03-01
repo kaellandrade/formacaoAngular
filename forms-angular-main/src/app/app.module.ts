bc5843c9838f351f5eea85624798cdf0d8552113
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-03-01 17:38:40 -0300

    feat? validando form

--- forms-angular-main/src/app/app.module.ts
+++ forms-angular-main/src/app/app.module.ts
@@ -8,17 +8,19 @@ import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CadastroComponent } from "./cadastro/cadastro.component";
 import { SucessoCadastroComponent } from "./sucesso-cadastro/sucesso-cadastro.component";
+import { MensagemComponent } from "./components/mensagem/mensagem.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    CadastroComponent,
-    SucessoCadastroComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		CadastroComponent,
+		SucessoCadastroComponent,
+		MensagemComponent
+	],
+	imports: [BrowserModule, AppRoutingModule, FormsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- forms-angular-main/src/app/cadastro/cadastro.component.ts
+++ forms-angular-main/src/app/cadastro/cadastro.component.ts
@@ -3,21 +3,21 @@ import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: "app-cadastro",
-  templateUrl: "./cadastro.component.html",
-  styleUrls: ["./cadastro.component.css"],
+	selector: "app-cadastro",
+	templateUrl: "./cadastro.component.html",
+	styleUrls: ["./cadastro.component.css"],
 })
 export class CadastroComponent implements OnInit {
-  constructor(private router: Router) {}
+	constructor(private router: Router) { }
 
-  public ngOnInit(): void {}
+	public ngOnInit(): void { }
 
-  public cadastrar(form: NgForm): void {
-    console.log(form.controls);
-    if (form.valid) {
-      this.router.navigate(["./sucesso"]);
-      return;
-    }
-    alert("Formulário inválido!");
-  }
+	public cadastrar(form: NgForm): void {
+		console.log(form.controls);
+		if (form.valid) {
+			this.router.navigate(["./sucesso"]);
+			return;
+		}
+		alert("Formulário inválido!");
+	}
 }
