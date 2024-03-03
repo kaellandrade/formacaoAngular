4ba985ec68a57236a068b0d4b8b6c9b29b3bb408
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-03-03 20:02:35 -0300

    feat: configurando service  cep

--- forms-angular-main/src/app/cadastro/cadastro.component.ts
+++ forms-angular-main/src/app/cadastro/cadastro.component.ts
@@ -1,23 +1,74 @@
 import { Router } from "@angular/router";
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { ConsultaCepService } from "../service/consulta-cep.service";
+import { Cep } from "../interfaces/cep";
 
 @Component({
-	selector: "app-cadastro",
-	templateUrl: "./cadastro.component.html",
-	styleUrls: ["./cadastro.component.css"],
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
 })
 export class CadastroComponent implements OnInit {
-	constructor(private router: Router) { }
+  @Input()
+  public cepData: Cep = {
+    cep: "",
+    logradouro: "",
+    complemento: "",
+    bairro: "",
+    localidade: "",
+    uf: "",
+    ibge: "",
+    gia: "",
+    ddd: "",
+    siafi: "",
+    erro: false,
+  };
 
-	public ngOnInit(): void { }
+  constructor(
+    private router: Router,
+    private serviceConsultarCep: ConsultaCepService
+  ) {}
 
-	public cadastrar(form: NgForm): void {
-		console.log(form.controls);
-		if (form.valid) {
-			this.router.navigate(["./sucesso"]);
-			return;
-		}
-		alert("Formulário inválido!");
-	}
+  public ngOnInit(): void {}
+
+  public cadastrar(form: NgForm): void {
+    console.log(form.controls);
+    if (form.valid) {
+      this.router.navigate(["./sucesso"]);
+      return;
+    }
+    alert("Formulário inválido!");
+  }
+
+  public consultarCep(form: NgForm) {
+    if (form?.controls?.["cep"].valid) {
+      this.cepData.cep = form.value["cep"];
+
+      this.serviceConsultarCep
+        .consultarCep(this.cepData.cep)
+        .subscribe((dado) => {
+          if (dado.erro) {
+            this.limparCep();
+            return;
+          }
+          this.cepData = <Cep>dado;
+        });
+    }
+  }
+  private limparCep(): void {
+    this.cepData = {
+      cep: "",
+      logradouro: "",
+      complemento: "",
+      bairro: "",
+      localidade: "",
+      uf: "",
+      ibge: "",
+      gia: "",
+      ddd: "",
+      siafi: "",
+      erro: false,
+    };
+  }
 }
