79eb4447f3d4d1f31fb6b35d348a942440d55892
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-03-07 15:38:40 -0300

    feat: iniciando app todo

---
+++ life-cycle-todo-app/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'app-lista-de-compras';
+  listaCompra!: Item[];
+
+  constructor(private listaSercice: ListaDeCompraService) {}
+  ngOnInit(): void {
+    this.listaCompra = this.listaSercice.getListaDeCompra();
+    console.log(this.listaCompra);
+  }
+}
