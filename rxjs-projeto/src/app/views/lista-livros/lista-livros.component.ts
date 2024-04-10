f297139e8a4df5fc718c25551da7bda01903a9d2
Micael Andrade <micael.santos@dcomp.ufs.br>
2024-04-10 17:53:59 -0300

    feature: utilizando operadores debounceTime, filter, distinctUntilChanged

--- rxjs-projeto/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-projeto/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, switchMap, tap } from 'rxjs';
+import {
+	debounceTime,
+	distinctUntilChanged,
+	filter,
+	map,
+	switchMap,
+	tap,
+} from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/LivroVolumeInfo';
 import { Item, Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
+const TAMANHO_MIN_BUSCA = 3;
+const DELAY_BUSCA = 500;
 @Component({
 	selector: 'app-lista-livros',
 	templateUrl: './lista-livros.component.html',
@@ -16,15 +25,18 @@ export class ListaLivrosComponent {
 	constructor(private serviceGoogleAPIBook: LivroService) {}
 
 	/*
+	TODO: Documentar os operadores
 	- Busca type ahead
 	- switchmap
 	- a ideia desse operador é trocaar os valores e passar ao servidor só o último valor 'D', desconsiderando os valores anteirores A,B,C
 	A -> B -> C -> D (apenas o D será o valor passado)
 	*/
 	livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
-		tap(() => console.log('Fluxo inicial')),
+		debounceTime(DELAY_BUSCA),
+		filter((valorDigital: string) => valorDigital.length >= TAMANHO_MIN_BUSCA), // filtrando para buscar apenas com 3 ou mais char
+		distinctUntilChanged(),
 		switchMap(valorDigitado => this.serviceGoogleAPIBook.buscar(valorDigitado)),
-		tap(() => console.log('Request')),
+		tap(resp => console.log(resp)),
 		map(items => items && this.parseToLivros(items))
 	);
 
@@ -40,7 +52,6 @@ export class ListaLivrosComponent {
 	}
 
 	private parseToLivros(items: Item[]): Livro[] {
-		console.log(items);
 		return items.reduce((acumulador: Livro[], atual: Item) => {
 			let livro: Livro = new LivroVolumeInfo(atual);
 			return acumulador.concat(livro);
