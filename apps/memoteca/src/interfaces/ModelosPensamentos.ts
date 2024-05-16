f9d20db8fa27cf127c5fbf4ef8365990551e35e8
Micael Andrade <mikael.java@gmail.com>
2024-05-16 14:11:25 -0300

    chore: migrando aplicação nx

--- apps/memoteca/src/interfaces/ModelosPensamentos.ts
+++
@@ -1,5 +0,0 @@
-export enum ModelosPensamentos {
-	MODELO1 = 'modelo1',
-	MODELO2 = 'modelo2',
-	MODELO3 = 'modelo3',
-}
--- apps/memoteca/src/interfaces/Pensamento.ts
+++
@@ -1,9 +0,0 @@
-import { ModelosPensamentos } from './ModelosPensamentos';
-
-export interface Pensamento {
-	id?: string;
-	conteudo: string;
-	autoria: string;
-	modelo: ModelosPensamentos;
-	favorito: boolean;
-}
