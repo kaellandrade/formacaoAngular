bcb4dc25961ac3fccdd45a5c7048bd39371c47d5
Micael Andrade <mikael.java@gmail.com>
2024-06-01 22:04:12 -0300

    testes: configurando testes e2e todo app

--- README.md
+++ README.md
@@ -52,6 +52,7 @@ https://github.com/kaellandrade/formacaoAngular/assets/51521476/fd632ff7-a7a8-4f
 ## Rodando os testes e2e de cada projeto com o Cypress: 
 - [App Memoteca](#memoteca) :iphone: - `npm run memoteca-e2e`;
 - [Buscador de Livos](#buscador-de-livos) :book: - `npm run buscante-e2e`;
+- [Todo APP](#todo-app) :white_check_mark: - `npm run todo-app-e2e`;
 
 <details>
 <summary>Mais detalhes sobre essa formação da Alura (Guideline)</summary>
---
+++ apps/life-cycle/cypress/e2e/todo-app.cy.ts
@@ -0,0 +1,50 @@
+const cadastrarTask = () => {
+  cy.get('[data-cy="input-task"]').as('inputTask');
+  cy.get('@inputTask').type('Estudar Estudar JS/TS {enter}');
+
+};
+
+describe('TODO-APP', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+    cy.visit('http://localhost:4200/');
+  });
+  it('Deve garantir a tela de listagem das task vazias', () => {
+    cy.contains('Ótimo, você não tem tarefas!');
+  });
+  it('Deve cadastrar uma nova tarefa', () => {
+    cadastrarTask();
+    cy.contains('Tarefa foi cadastrada!');
+  });
+  it('Deve atualizar uma tarefa garantido que os botões de ação das task fiquem disabled', () => {
+    cadastrarTask();
+    cy.get('[data-cy="action-btn-edit-1"').as('btn-edit');
+    cy.get('@btn-edit').click();
+    cy.get('[data-cy="input-task"]').clear();
+    cy.focused().type('Estudar JavaScript e TapeScript');
+    cy.get('@btn-edit').should('have.class', 'p-disabled');
+    cy.get('[data-cy="1"]').should('have.attr', 'ng-reflect-disabled', 'true');
+    cy.get('[data-cy="bt-apagar-tudo"]').should('have.class', 'p-disabled');
+    cy.get('[data-cy="submit-btn"]').click();
+    cy.contains('Tarefa foi editada!');
+  });
+
+  it('Deve apagar uma task', () => {
+    cadastrarTask();
+    cy.get('[data-cy="action-btn-trash-1"').click();
+    cy.get('.p-dialog-footer > .p-button-danger').click();
+    cy.contains('Tarefa foi deletada!');
+  });
+
+  it('Deve cadastrar várias tarefas e apagar todas', () => {
+    cadastrarTask();
+    cadastrarTask();
+    cadastrarTask();
+    cadastrarTask();
+    cadastrarTask();
+    cy.get('[data-cy="bt-apagar-tudo"]').click();
+    cy.get('.p-dialog-footer > .p-button-danger').click();
+    cy.contains('Todas suas tarefas foram deletadas!');
+
+  });
+});
