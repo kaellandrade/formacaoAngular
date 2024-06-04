6340f2612931ae2e0b9c33fdac14d12624bc168b
Micael Andrade <mikael.java@gmail.com>
2024-06-04 15:54:31 -0300

    feature: configurando rotas

--- apps/memorando/src/app/app.module.ts
+++ apps/memorando/src/app/app.module.ts
@@ -1,15 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
 
+import { CabecalhoComponent } from '../../components/cabecalho/Cabecalho.component';
+import { ListaTarefasComponent } from '../../components/listar-tarefas/ListaTarefas.component';
+import { MensagemComponent } from '../../components/mensagem/Mensagem.component';
+import { RodapeComponent } from '../../components/rodape/Rodape.component';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AppRouteReuseStrategy } from './app-route-reuse-strategy';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [],
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    MensagemComponent,
+    RodapeComponent,
+    ListaTarefasComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes, { onSameUrlNavigation: 'reload' }),
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: AppRouteReuseStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/memorando/src/app/app.routes.ts
+++ apps/memorando/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+import { ListaTarefasComponent } from '../../components/listar-tarefas/ListaTarefas.component';
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'listaTarefas',
+    pathMatch: 'full',
+    data: {
+      reuseComponent: true,
+    },
+  },
+  {
+    path: 'listaTarefas',
+    component: ListaTarefasComponent,
+    data: {
+      reuseComponent: true,
+    },
+  },
+];
