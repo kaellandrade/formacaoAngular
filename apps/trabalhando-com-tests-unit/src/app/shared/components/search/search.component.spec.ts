aec5c235d1284ed30a87f9a7a806c088f49c5b5d
Micael Andrade <mikael.java@gmail.com>
2025-03-17 10:24:35 -0300

    test: finalizando testes componentes dumbs

--- apps/trabalhando-com-tests-unit/src/app/shared/components/search/search.component.spec.ts
+++ apps/trabalhando-com-tests-unit/src/app/shared/components/search/search.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SearchComponent } from './search.component';
 
-describe('SearchComponent', () => {
+fdescribe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchComponent],
+      imports: [SearchComponent, BrowserAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
@@ -18,5 +20,24 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be emitted event search', () => {
+    const spy = spyOn(component.searchText, 'emit');
+    const inputElement = fixture.debugElement.query(
+      By.css('[data-testid="search-input"]'),
+    ).nativeElement as HTMLInputElement;
+
+    inputElement.value = 'Smartphone';
+    inputElement.dispatchEvent(new Event('input')); // simula digitação
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalledWith('Smartphone');
+  });
+  it('should be created button search', () => {
+    const btnElement = fixture.debugElement.query(
+      By.css('[data-testid="search-btn"]'),
+    ).nativeElement as HTMLButtonElement;
+    expect(btnElement).toBeTruthy();
   });
 });
