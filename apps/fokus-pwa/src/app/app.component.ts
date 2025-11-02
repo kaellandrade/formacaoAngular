2e8afdc374189cd75aa22c36de2aca041fc42902
Micael Andrade <mikael.java@gmail.com>
2025-11-02 13:58:31 -0300

    chore: configurando pwa

--- apps/fokus-pwa/src/app/app.component.ts
+++ apps/fokus-pwa/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  standalone: false,
 })
 export class AppComponent {
   title = 'fokus-pwa';
